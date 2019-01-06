5aa9223df4473ba3caec566be044aea2ee6023a8
brooke <brooke.mitchell@comcast.net>
2019-01-05 20:39:03 -0500

    edit pages

---
+++ src/components/SavedJumbo/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      className="jumbotron"
+    >
+      <h1>Saved Books</h1>
+      <h4> you better start reading!</h4>
+    </div>
+  );
+}
+
+export default Jumbotron;
