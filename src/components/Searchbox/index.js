c910ce2fdb66fc4eaf6da3364843391b77555ad8
brooke <brooke.mitchell@comcast.net>
2019-01-05 20:21:43 -0500

    its bunk

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -5,10 +5,10 @@ function Searchbox() {
       <div className="container">
     <div className="form-group">
      <label for="exampleFormControlInput1">Search for a book</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="The Cat in the Hat">
+    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="The Cat in the Hat">
     </input>
     </div>
-    <button type="submit" class="btn btn-primary float-right">Submit</button>
+    <button type="submit" className="btn btn-primary float-right">Submit</button>
     </div>
   );
 }
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,17 +1,75 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav";
 import Jumbotron from "../components/Jumbotron";
-import Searchbox from "../components/Searchbox";
-
+import API from "../utils/API";
+import { Input, FormBtn } from "../components/Form";
 
 class Search extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.saveBook({
+      title: this.state.title
+    })
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
-     <div>
-       <Nav />
-       <Jumbotron />
-       <Searchbox />
-    </div>
+      <div>
+        <Nav />
+        <Jumbotron />
+        <div className="container">
+          <form>
+            <Input
+              value={this.state.title}
+              onChange={this.handleInputChange}
+              name="title"
+              placeholder="The Cat In The Hat"
+            />
+
+            
+            <FormBtn
+              disabled={!this.state.title}
+              onClick={this.handleFormSubmit}
+            >
+              Search
+            </FormBtn>
+          </form>
+        </div>
+      </div>
     );
   }
 }
